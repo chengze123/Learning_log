400cf1d9fbdecb06a58ce80c9bdb782e58f4916c
Daisy <987190490@qq.com>
2021-02-25 23:11:38 +0800

    Delete 2.14修改.py

--- 2.14修改.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-res = requests.get('http://www.jwview.com/jingwei/html/02-10/381329.shtml')
-res.encoding = 'gbk'
-#print(res.text)
-soup = BeautifulSoup(res.text,'html.parser')
-#soup.select('title')[0].text
-print(soup)
